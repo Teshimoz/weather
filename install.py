b640252ebfe7b7211eb91db51278e9ff2ad00ad4
Teshimoz <51529116+Teshimoz@users.noreply.github.com>
2022-05-11 23:39:17 +0300

    Update install.py
    added fields for second BME sensor and for CO2 meter

--- install.py
+++ install.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Original from http://www.avislab.com/blog/raspberry-pi-meteo_ru/
+# Edited by Teshimoz - 2018, 2022
 
 import sqlite3
 
@@ -7,7 +9,7 @@ print "Database creating..."
 
 con = sqlite3.connect('weather.db')
 cur = con.cursor()
-cur.execute('CREATE TABLE weather (id REAL PRIMARY KEY, temperature REAL, pressure REAL, Humidity REAL)')
+cur.execute('CREATE TABLE weather (id REAL PRIMARY KEY, temperature REAL, pressure REAL, Humidity REAL, temperature_indoor REAL, pressure_indoor REAL, Humidity_indoor REAL, temperature_delta REAL, rainstate REAL, co2 INTEGER)')
 con.commit()
 con.close()
 
